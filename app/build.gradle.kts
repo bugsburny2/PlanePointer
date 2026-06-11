ef6226ef42587f485bffa30f7a1799115068b61d
PointToPlaneWear Developer <developer@pointtoplane.wear>
2026-06-11 22:47:04 +0200

    Configure lint options to ignore release build errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,10 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = false
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
